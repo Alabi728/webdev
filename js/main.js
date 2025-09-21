35ceb8d0e3af743c685d158faf6077c83199d082
Alabi <halabee2023@gmail.com>
2025-09-21 15:36:03 +0100

    style: refactor greeting function to consolidate alert messages and improve readability

--- js/main.js
+++ js/main.js
@@ -14,19 +14,13 @@ function greeting3() {
     var teamName = document.getElementById("team-4").innerText;
     alert("Team member :  " + teamName + "");
 }
+        
 function greeting4() {
     var userName = document.getElementById("input-name").value;
-    alert("Name: " + userName + "");
-}
-function greeting4() {
-    var userEmail = document.getElementById("input-email").value;
-    alert("Email: " + userEmail + "");
-}
-function greeting4() {
-    var userSubject = document.getElementById("input-subject").value;
-    alert("Subject: " + userSubject + "");
-}
-function greeting4() {
+        var userEmail = document.getElementById("input-email").value;
+        var userSubject = document.getElementById("input-subject").value;
     var userComment = document.getElementById("input-comment").value;
-    alert("Comment: " + userComment + "");
+    alert("Name: " + userName + "\nEmail: " + userEmail + "" + "\nSubject: "
+        + userSubject + "" + "\nComment: " + userComment + ""
+    )
 }
