6604d24b61776a3b8510c56c1e85e04903b58812
Alabi <halabee2023@gmail.com>
2025-09-21 15:16:09 +0100

    style: update form inputs with IDs and enhance greeting functionality

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
- function greeting() {
-            var teamName = document.getElementById("team-1").innerText;
-            alert("Team member :  " + teamName + "");
-        }
-        function greeting1() {
-            var teamName = document.getElementById("team-3").innerText;
-            alert("Team member :  " + teamName + "");
-        }
-        function greeting2() {
-            var teamName = document.getElementById("team-2").innerText;
-            alert("Team member :  " + teamName + "");
-        }
-        function greeting3() {
-            var teamName = document.getElementById("team-4").innerText;
-            alert("Team member :  " + teamName + "");
-        }
+function greeting() {
+    var teamName = document.getElementById("team-1").innerText;
+    alert("Team member :  " + teamName + "");
+}
+function greeting1() {
+    var teamName = document.getElementById("team-3").innerText;
+    alert("Team member :  " + teamName + "");
+}
+function greeting2() {
+    var teamName = document.getElementById("team-2").innerText;
+    alert("Team member :  " + teamName + "");
+}
+function greeting3() {
+    var teamName = document.getElementById("team-4").innerText;
+    alert("Team member :  " + teamName + "");
+}
+function greeting4() {
+    var userName = document.getElementById("input-name").value;
+    alert("Name: " + userName + "");
+}
+function greeting4() {
+    var userEmail = document.getElementById("input-email").value;
+    alert("Email: " + userEmail + "");
+}
+function greeting4() {
+    var userSubject = document.getElementById("input-subject").value;
+    alert("Subject: " + userSubject + "");
+}
+function greeting4() {
+    var userComment = document.getElementById("input-comment").value;
+    alert("Comment: " + userComment + "");
+}
