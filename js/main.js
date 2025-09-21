a370c756de733b47892165ac557f2a97ef711df9
Alabi <halabee2023@gmail.com>
2025-09-21 14:08:40 +0100

    style: add responsive team button and improve team member interaction

---
+++ js/main.js
@@ -0,0 +1,16 @@
+ function greeting() {
+            var teamName = document.getElementById("team-1").innerText;
+            alert("Team member :  " + teamName + "");
+        }
+        function greeting1() {
+            var teamName = document.getElementById("team-3").innerText;
+            alert("Team member :  " + teamName + "");
+        }
+        function greeting2() {
+            var teamName = document.getElementById("team-2").innerText;
+            alert("Team member :  " + teamName + "");
+        }
+        function greeting3() {
+            var teamName = document.getElementById("team-4").innerText;
+            alert("Team member :  " + teamName + "");
+        }
